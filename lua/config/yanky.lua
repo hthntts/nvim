699692562b287a39076d77d1a6be72918f852a9f
Hai Ho <hothienhaidl@gmail.com>
2024-12-31 18:54:00 +0700

    [yanky] update

--- lua/config/yanky.lua
+++ lua/config/yanky.lua
@@ -9,9 +9,8 @@ require("yanky").setup {
   },
 }
 
-vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-
+-- vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+-- vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
 -- cycle through the yank history, only work after paste
-vim.keymap.set("n", "[y", "<Plug>(YankyPreviousEntry)")
-vim.keymap.set("n", "]y", "<Plug>(YankyNextEntry)")
+-- vim.keymap.set("n", "[y", "<Plug>(YankyPreviousEntry)")
+-- vim.keymap.set("n", "]y", "<Plug>(YankyNextEntry)")
