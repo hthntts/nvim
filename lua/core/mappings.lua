5e72b16fc8a4c7b9e1ed3d8b3bddd4d4c543fad0
Hai Ho <hothienhaidl@gmail.com>
2024-12-31 18:54:34 +0700

    [mapping] update

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -69,12 +69,12 @@ keymap.set("n", "<leader>bd", "<cmd>bd<cr>", {
 
 -- Insert a blank line below or above current line (do not move the cursor),
 -- see https://stackoverflow.com/a/16136133/6064933
-keymap.set("n", "<space>lo", "printf('m`%so<ESC>``', v:count1)", {
+keymap.set("n", "<leader>lo", "printf('m`%so<ESC>``', v:count1)", {
   expr = true,
   desc = "Insert blank line below",
 })
 
-keymap.set("n", "<space>lO", "printf('m`%sO<ESC>``', v:count1)", {
+keymap.set("n", "<leader>lO", "printf('m`%sO<ESC>``', v:count1)", {
   expr = true,
   desc = "Insert blank line above",
 })
