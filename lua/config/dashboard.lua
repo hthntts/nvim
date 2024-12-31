503843163cf7b53d74eea519d87630fcfaba584c
Hai Ho <hothienhaidl@gmail.com>
2024-12-31 18:54:24 +0700

    [update] dashboard

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,69 +1,61 @@
-local api = vim.api
-local keymap = vim.keymap
-local dashboard = require("dashboard")
+local dashboard = require('dashboard')
+local icons = require('config/icons')
 
-local conf = {}
-conf.header = {
-  "                                                       ",
-  "                                                       ",
-  "                                                       ",
-  "                                                       ",
-  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-  "                                                       ",
-  "                                                       ",
-  "                                                       ",
-  "                                                       ",
-}
-
-conf.center = {
-  {
-    icon = "󰈞  ",
-    desc = "Find File                               ",
-    action = "Leaderf file --popup",
-    key = "SPC f f",
-  },
-  {
-    icon = "󰈢  ",
-    desc = "Recently Opened Files                   ",
-    action = "Leaderf mru --popup",
-    key = "SPC f r",
-  },
-  {
-    icon = "󰈬  ",
-    desc = "Project Grep                            ",
-    action = "Leaderf rg --popup --no-messages --nameOnly",
-    key = "SPC /",
-  },
-  {
-    icon = "  ",
-    desc = "New File                                ",
-    action = "enew",
-    key = "e",
-  },
-  {
-    icon = "󰗼  ",
-    desc = "Quit Nvim                               ",
-    action = "qa",
-    key = "q",
-  },
-}
-
-dashboard.setup {
-  theme = "doom",
-  shortcut_type = "number",
-  config = conf,
-}
-
-api.nvim_create_autocmd("FileType", {
-  pattern = "dashboard",
-  group = api.nvim_create_augroup("dashboard_enter", { clear = true }),
-  callback = function()
-    keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
-    keymap.set("n", "e", ":enew<CR>", { buffer = true, silent = true })
-  end,
+dashboard.setup({
+    theme = 'hyper',
+    disable_move = false,
+    shortcut_type = 'letter',
+    change_to_vcs_root = true,
+    hide = {
+        statusline = true,
+        tabline = false,
+        winbar = false,
+    },
+    config = {
+        week_header = {
+            enable = true,
+            append = { '', icons.ui.Circle .. ' Tux: Learn, Make, Explore! ' .. icons.ui.Circle },
+        },
+        packages = { enable = true },
+        project = { enable = true, limit = 12, action = 'Telescope find_files cwd=' },
+        mru = { limit = 12 },
+        shortcut = {
+            {
+                desc = icons.ui.Package .. 'Project',
+                group = '@property',
+                action = 'NeovimProjectDiscover',
+                key = 'p',
+            },
+            {
+                desc = icons.documents.OpenFolder .. 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'f',
+            },
+            {
+                desc = icons.ui.Search .. 'Search',
+                group = 'Label',
+                action = 'Telescope live_grep',
+                key = 'g',
+            },
+            {
+                desc = icons.ui.Power,
+                group = 'Action',
+                action = 'quit',
+                key = 'q',
+            },
+        },
+        footer = { '', icons.ui.Heart .. ' Nothing to say ' .. icons.ui.Heart },
+    },
 })
+
+-- Show dashboard when lazy closes
+if vim.o.filetype == 'lazy' then
+    vim.cmd.close()
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'DashboardLoaded',
+        callback = function()
+            require('lazy').show()
+        end,
+    })
+end
