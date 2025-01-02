3f79de5218fc2cf511d4949242a6708fc01d1553
Hai Ho <hothienhaidl@gmail.com>
2025-01-03 01:04:53 +0700

    update bufferline

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -33,14 +33,14 @@ require("bufferline").setup {
 
       return true
     end,
-    show_buffer_icons = false,
+    show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     separator_style = "bar",
     enforce_regular_tabs = false,
-    always_show_bufferline = true,
+    always_show_bufferline = false,
     sort_by = "id",
   },
 }
