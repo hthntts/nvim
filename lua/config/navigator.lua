82e7bacbc816d5adfe6aeee29b62a748fc4999fc
Hai Ho <hothienhaidl@gmail.com>
2025-01-03 13:56:57 +0700

    update navigator

--- lua/config/navigator.lua
+++ lua/config/navigator.lua
@@ -4,3 +4,9 @@ require("Navigator").setup {
   mux = 'auto',
 }
 
+if vim.fn.exists('$TMUX') == 1 then
+  keymap.set({"n", "i", "x"}, "<A-h>", "<cmd>NavigatorLeft<cr>", { silent = true, desc = "Navigator Left" })
+  keymap.set({"n", "i", "x"}, "<A-j>", "<cmd>NavigatorDown<cr>", { silent = true, desc = "Navigator Down" })
+  keymap.set({"n", "i", "x"}, "<A-k>", "<cmd>NavigatorUp<cr>", { silent = true, desc = "Navigator Up" })
+  keymap.set({"n", "i", "x"}, "<A-l>", "<cmd>NavigatorRight<cr>", { silent = true, desc = "Navigator Right" })
+end
