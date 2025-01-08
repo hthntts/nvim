0a0657edeb75ead754fa2848952878d60ac903bb
Hai Ho <hothienhaidl@gmail.com>
2025-01-08 23:09:06 +0700

    neogit remove plugins unused

--- lua/git/neogit.lua
+++ lua/git/neogit.lua
@@ -5,8 +5,6 @@ return {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
       "nvim-telescope/telescope.nvim",
-      "ibhagwan/fzf-lua",
-      "echasnovski/mini.pick",
     },
     config = function()
       local neogit = require("neogit")
