195bdce98c889e1425ff6d8b8e1306264871e6cd
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:11:56 +0700

    update bufferline.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -26,6 +26,7 @@ return {
         icon = "▎",
         style = "icon",
       },
+
       buffer_close_icon = icons.ui.Close,
       modified_icon = icons.ui.CircleSmall,
       close_icon = icons.ui.Close,
@@ -46,8 +47,9 @@ return {
 
         return true
       end,
+
       show_buffer_icons = true,
-      show_buffer_close_icons = true,
+      show_buffer_close_icons = false,
       show_close_icon = true,
       show_tab_indicators = true,
       persist_buffer_sort = true,
