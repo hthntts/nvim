f2eaa69eb42227e08a29e59f43077dfa75e4c784
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:15:46 +0700

    add multicursors

---
+++ lua/plugins/multicursors.lua
@@ -0,0 +1,37 @@
+return {
+  "smoka7/multicursors.nvim",
+  event = 'VeryLazy',
+  dependencies = 'nvimtools/hydra.nvim',
+  cmd = {
+    'MCstart',
+    'MCvisual',
+    'MCclear',
+    'MCpattern',
+    'MCvisualPattern',
+    'MCunderCursor'
+  },
+  keys = {
+    {
+      mode = { 'v', 'n' },
+      '<M-d>',
+      '<cmd>MCvisual<cr>',
+      desc = 'Multi Cursors',
+    },
+  },
+  opts = {
+    hint_config = {
+      float_opts = {
+        border = 'rounded',
+      },
+      position = 'bottom-right',
+    },
+    generate_hints = {
+      normal = true,
+      insert = true,
+      extend = true,
+      config = {
+        column_count = 1,
+      },
+    }
+  },
+}
