10ad91caa1703cc2107e57c323f631c5ac865f30
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:13:50 +0700

    update which-key.lua

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("which-key").setup {
       -- classic, modern, helix
-      preset = "classic",
+      preset = "modern",
       delay = 0,
       icons = {
         mappings = false,
@@ -13,6 +13,7 @@ return {
 
     local wk = require("which-key")
     wk.add({
+      { "<leader>b",  group = "Buffers" },
       { "<leader>c",  group = "Code" },
       { "<leader>co", group = "Object" },
       { "<leader>cq", group = "Quickfix" },
@@ -21,26 +22,17 @@ return {
       { "<leader>g",  group = "Git" },
       { "<leader>gc", group = "Create" },
       { "<leader>go", group = "Open" },
-      { "<leader>p",  group = "Project" },
       { "<leader>h",  group = "Help" },
       { "<leader>i",  group = "Insert" },
+      { "<leader>l",  group = "Lazy" },
+      { "<leader>m",  group = "Markdown" },
       { "<leader>o",  group = "Open" },
+      { "<leader>p",  group = "Project" },
       { "<leader>q",  group = "Quit" },
       { "<leader>r",  group = "Reload" },
       { "<leader>s",  group = "Search" },
-      { "<leader>u",  group = "U" },
       { "<leader>t",  group = "Toggle" },
-      { "<leader>z",  group = "Folds" },
       { "<leader>w",  proxy = "<c-w>",   group = "Windows" },
-      {
-        "<leader>b",
-        group = "Buffers",
-        expand = function()
-          return require("which-key.extras").expand.buf()
-        end
-      },
-      { "<leader>om", "<cmd>MarkdownPreview<cr>",     desc = "Markdown preview" },
-      { "<leader>os", "<cmd>MarkdownPreviewStop<cr>", desc = "Stop Markdown preview" },
     })
   end,
 }
