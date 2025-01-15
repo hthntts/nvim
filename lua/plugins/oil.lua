d19e582cc45868666d5692134e7945662f77d8fb
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:15:28 +0700

    add oil.lua

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  event = 'VeryLazy',
+  dependencies = "echasnovski/mini.icons",
+  cmd = "Oil",
+  keys = {
+    { "<leader>oo", "<cmd>Oil<cr>", desc = "Oil" },
+  },
+  opts = {
+    delete_to_trash = true,
+    keymaps = {
+      ["?"] = { "actions.show_help", mode = "n" },
+      ["q"] = { "actions.close", mode = "n" },
+    },
+
+  },
+}
