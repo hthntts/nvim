6f30acf0e0d1f45d26517b582c1f8a5cfeb4f595
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:13:22 +0700

    update notify.lua

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,13 +2,12 @@ return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
   opts = {
-    -- Animation style
     stages = "fade_in_slide_out",
-    -- Default timeout for notifications
     timeout = 1500,
     background_colour = "#2E3440",
   },
   config = function()
-    vim.notify = require("notify")
+    local notify = require('notify')
+    vim.notify = notify
   end,
 }
