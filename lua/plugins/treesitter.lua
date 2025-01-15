4141c0bc778ede1d3a3f169dbb9aface1ee6fa51
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:13:38 +0700

    update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = 'BufReadPre',
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
+  dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
   config = function()
     require("nvim-treesitter.configs").setup({
       -- Add languages to be installed here that you want installed for treesitter
