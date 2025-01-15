52f3be18647edfa19503e0d338d1268209690806
Hai Ho <hothienhaidl@gmail.com>
2025-01-15 23:14:57 +0700

    add eunuch.lua

---
+++ lua/plugins/eunuch.lua
@@ -0,0 +1,22 @@
+return {
+  'tpope/vim-eunuch',
+  cmd = {
+    'Delete',
+    'Unlink',
+    'Remove',
+    'Copy',
+    'Duplicate',
+    'Move',
+    'Rename',
+    'Chmod',
+    'Mkdir',
+    'Cfind',
+    'Lfind',
+    'Clocate',
+    'Llocate',
+    'SudoEdit',
+    'SudoWrite',
+    'Wall',
+    'W',
+  }
+}
