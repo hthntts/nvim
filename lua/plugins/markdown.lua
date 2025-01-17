160d7b3738d92803344ce6e43b0a5d07f16d70fe
Hai Ho <hothienhaidl@gmail.com>
2025-01-17 15:39:11 +0700

    add helpview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -73,4 +73,11 @@ return {
     },
     ft = { 'markdown' }
   },
+
+  {
+    'OXY2DEV/helpview.nvim',
+    lazy = false, -- Helpview handles lazy loading
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+  },
+
 }
