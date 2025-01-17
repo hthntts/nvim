68841c58d3a1d6e2f28a5ebb6f7d77065503a9f9
Hai Ho <hothienhaidl@gmail.com>
2025-01-17 12:27:04 +0700

    update notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,23 +2,10 @@ return {
   'rcarriga/nvim-notify',
   event = 'VeryLazy',
   config = function()
+    require("notify").setup({
+      background_colour = '#45475a',
+    })
     local notify = require('notify')
-
-    ---@diagnostic disable-next-line: missing-fields
-    notify.setup({ timeout = 2000 })
-
     vim.notify = notify
-
-    -- LSP window/showMessage
-    vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
-      local client = vim.lsp.get_client_by_id(ctx.client_id)
-      local level = ({ 'ERROR', 'WARN', 'INFO', 'DEBUG' })[result.type]
-
-      notify({ result.message }, level, {
-        title = 'LSP | ' .. (client and client.name or '?'),
-        timeout = 10000,
-        keep = function() return level == 'ERROR' or level == 'WARN' end,
-      })
-    end
   end
 }
