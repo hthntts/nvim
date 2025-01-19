f5dc2b8cf6da69e7a3b7566f2acc38775e26b64c
Hai Ho <hothienhaidl@gmail.com>
2025-01-19 17:35:00 +0700

    add treesj

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,17 @@
+local function toggle()
+  require('treesj').toggle()
+end
+
+return {
+  'Wansmer/treesj',
+  dependencies = 'nvim-treesitter',
+  keys = {
+    { 'gS', toggle, desc = 'Split text-object into multiple lines' },
+  },
+  config = function()
+    require('treesj').setup({
+      use_default_keymaps = false,
+      max_join_length = 500,
+    })
+  end
+}
