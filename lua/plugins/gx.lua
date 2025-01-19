f5c5424b0c1b3086f3e00bfb9b8945487881bf02
Hai Ho <hothienhaidl@gmail.com>
2025-01-20 00:45:14 +0700

    update key gx.lua

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -4,7 +4,7 @@ return {
   dependencies = 'nvim-lua/plenary.nvim',
   cmd = 'Browse',
   keys = {
-    { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' }, desc = 'Open link (search current word)' },
+    { '<leader>ob', '<cmd>Browse<cr>', mode = { 'n', 'x' }, desc = 'Open link (search current word)' },
   },
   opts = {
     handlers = { search = true },
