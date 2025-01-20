007f2f241bb4afb0fb96028851a9712c10474f7c
Hai Ho <hothienhaidl@gmail.com>
2025-01-20 14:20:45 +0700

    disable hardtime

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -131,11 +131,11 @@ return {
     end
   },
 
-  -- Establish good command workflow and quit bad habit
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = 'MunifTanjim/nui.nvim',
-    opts = {}
-  },
+  -- -- Establish good command workflow and quit bad habit
+  -- {
+  --   'm4xshen/hardtime.nvim',
+  --   dependencies = 'MunifTanjim/nui.nvim',
+  --   opts = {}
+  -- },
 
 }
