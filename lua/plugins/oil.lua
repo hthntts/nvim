b91e2ab4791b7f734118a1ab9df160a13866ffea
Hai Ho <hothienhaidl@gmail.com>
2025-01-24 14:13:42 +0700

    update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,9 +4,45 @@ return {
   dependencies = 'echasnovski/mini.icons',
   cmd = 'Oil',
   keys = {
-    { '<leader>oo', '<cmd>Oil<cr>', desc = 'Oil' },
+    { '<leader>oo', '<cmd>Oil --float<cr>', desc = 'Oil' },
   },
   opts = {
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+      -- This function defines what is considered a "hidden" file
+      is_hidden_file = function(name, bufnr)
+        local m = name:match '^%.'
+        return m ~= nil
+      end,
+      -- This function defines what will never be shown, even when `show_hidden` is set
+      is_always_hidden = function(name, bufnr)
+        return false
+      end,
+      -- Sort file names with numbers in a more intuitive order for humans.
+      -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+      natural_order = 'fast',
+      -- Sort file and directory names case insensitive
+      case_insensitive = false,
+      sort = {
+        -- sort order can be "asc" or "desc"
+        -- see :help oil-columns to see which columns are sortable
+        { 'type', 'asc' },
+        { 'name', 'asc' },
+      },
+    },
+    -- Configuration for the floating window in oil.open_float
+    float = {
+      -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      get_win_title = nil,
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = 'right',
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
     delete_to_trash = true,
     keymaps = {
       ['?'] = { 'actions.show_help', mode = 'n' },
